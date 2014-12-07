1e126a98ea56e27cde81cbe00279aab507ba5006
Jason Thrasher <jasonthrasher@gmail.com>
2014-12-07 01:08:44 +0000

    merging pihole into setup, so dnsmasq is configured in one place

--- setup.sh
+++ setup.sh
@@ -192,6 +192,59 @@ grep "Sorry. You are not using Tor." tor-check.txt
 
 }
 
+function dnshole {
+  echo "Setting up DNSMasq"
+  sudo apt-get autoremove isc-dhcp-server
+  sudo apt-get install -y dnsmasq dnsutils
+
+  # see  /etc/dnsmasq.conf for more config options
+  echo "conf-dir=/etc/dnsmasq.d" >> /etc/dnsmasq.conf
+
+  # Any files added to the directory /etc/dnsmasq.d are automatically loaded by dnsmasq after a restart.
+  cat >> /etc/dnsmasq.d/dnsmasq.custom.conf << EOF
+interface=wlan1
+#except-interface=wlan0
+bind-interfaces
+
+no-resolv
+listen-address=192.168.42.1
+server=192.168.42.1#9053
+
+log-dhcp
+log-queries
+
+dhcp-range=wlan1,192.168.42.10,192.168.42.50,2h
+
+# Gateway
+dhcp-option=3,192.168.42.1
+
+# DNS
+dhcp-option=6,192.168.42.1
+
+dhcp-authoritative
+EOF
+
+  cp -ap /etc/resolv.conf /etc/resolv.conf.bak
+  cat > /etc/resolv.conf << EOF
+nameserver 192.168.42.1
+#nameserver 8.8.8.8
+#nameserver 8.8.4.4
+EOF
+
+  update-rc.d dnsmasq enable
+
+  echo "Setup weekly cron to update ad-hosts"
+  cp dnsmasq.adlist.sh /etc/cron.weekly/.
+  /etc/cron.weekly/dnsmasq.adlist.sh
+
+  echo "restart DNSMasq to pickup new config"
+  service dnsmasq restart
+
+  echo "test on a few domains"
+  dig adafruit.com
+  dig doubleclick.net
+}
+
 case "$1" in
   clean)
     clean
