e0a5ab1cfb74fbe58b95164b81d73ca8864683bb
Jason Thrasher <jasonthrasher@gmail.com>
2014-12-07 03:53:11 +0000

    build nginx in it's own file

--- setup.sh
+++ setup.sh
@@ -432,76 +432,6 @@ EOF
   dig doubleclick.net
 }
 
-function build_nginx {
-  echo "Building nginx ..."
-  # names of latest versions of each package
-  VERSION_PCRE=pcre-8.36
-  VERSION_OPENSSL=openssl-1.0.1j
-  VERSION_NGINX=nginx-1.7.8
-
-  # URLs to the source directories
-  SOURCE_OPENSSL=https://www.openssl.org/source/
-  SOURCE_PCRE=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/
-  SOURCE_NGINX=http://nginx.org/download/
-
-  # clean out any files from previous runs of this script
-  rm -rf build
-  mkdir build
-
-  # ensure that we have the required software to compile our own nginx
-  sudo apt-get -y install curl wget build-essential
-
-  # grab the source files
-  wget -P ./build $SOURCE_PCRE$VERSION_PCRE.tar.gz
-  wget -P ./build $SOURCE_OPENSSL$VERSION_OPENSSL.tar.gz --no-check-certificate
-  wget -P ./build $SOURCE_NGINX$VERSION_NGINX.tar.gz
-
-  # expand the source files
-  cd build
-  tar xzf $VERSION_NGINX.tar.gz
-  tar xzf $VERSION_OPENSSL.tar.gz
-  tar xzf $VERSION_PCRE.tar.gz
-  cd ../
-
-  # set where OpenSSL and nginx will be built
-  BPATH=$(pwd)/build
-  STATICLIBSSL="$BPATH/staticlibssl"
-
-  # build static openssl
-  cd $BPATH/$VERSION_OPENSSL
-  rm -rf "$STATICLIBSSL"
-  mkdir "$STATICLIBSSL"
-  make clean
-  ./config --prefix=$STATICLIBSSL no-shared \
-  && make depend \
-  && make \
-  && make install_sw
-
-  # build nginx, with various modules included/excluded
-  cd $BPATH/$VERSION_NGINX
-  mkdir -p $BPATH/nginx
-  ./configure --with-cc-opt="-I $STATICLIBSSL/include -I/usr/include" \
-  --with-ld-opt="-L $STATICLIBSSL/lib -Wl,-rpath -lssl -lcrypto -ldl -lz" \
-  --sbin-path=/usr/sbin/nginx \
-  --conf-path=/etc/nginx/nginx.conf \
-  --pid-path=/var/run/nginx.pid \
-  --error-log-path=/var/log/nginx/error.log \
-  --http-log-path=/var/log/nginx/access.log \
-  --with-pcre=$BPATH/$VERSION_PCRE \
-  --with-http_ssl_module \
-  --with-http_spdy_module \
-  --with-file-aio \
-  --with-ipv6 \
-  --with-http_gzip_static_module \
-  --with-http_stub_status_module \
-  --without-mail_pop3_module \
-  --without-mail_smtp_module \
-  --without-mail_imap_module \
-  && make && make install
-
-  echo "All done building nginx.";
-}
-
 case "$1" in
   clean)
     clean
@@ -510,7 +440,6 @@ case "$1" in
     setup_access_point $2 $3
     install_tor
     dnshole
-    build_nginx
     ;;
   *)
     echo "Usage: $0 { clean | install [SSID] [PSK] }"
